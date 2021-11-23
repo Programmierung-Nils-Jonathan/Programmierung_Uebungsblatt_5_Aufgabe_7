2b2a668332c18791f04fcddd9eec4d8c03a2a3be
Jonathan Treffler <jonathan.treffler@rwth-aachen.de>
2021-11-23 18:24:52 +0000

    added init files

---
+++ HTree.java
@@ -0,0 +1,26 @@
+public class HTree {
+	public class HTree {
+	private Canvas c;
+	
+	public static void main(String[] args) {
+		HTree t=new HTree();
+		t.drawHTree(600,5);
+	}
+	
+	public HTree(){
+		c=new Canvas(); //Ausrichtung ist bei Neuerstellung nach unten
+	}
+	
+	/*Diese Methode zeichnet einen H-Tree der Tiefe n. Vor dem Aufruf der Methode 
+	muss sichergestellt sein,
+	dass die Canvas Zeichenrichtung nach unten gerichtet ist. */ 
+	
+	/*Diese Methode zeichnet einen H-Tree der Tiefe n, vor dem Aufrufen der Methode 
+	muss sichergestellt sein,
+	dass die Canvas Zeichenrichtung nach unten gerichtet ist. */ 
+	public void drawHTree(int size, int n) {
+		//TODO
+	}
+	
+	
+}
