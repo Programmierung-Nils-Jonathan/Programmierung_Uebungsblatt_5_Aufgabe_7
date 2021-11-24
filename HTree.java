9979efdfd4c53ef168d6a69403c44c69a4cf43b2
Jonathan Treffler <mail@jonathan-treffler.de>
2021-11-24 20:53:43 +0100

    integrated HTree2 into HTree

--- HTree.java
+++ HTree.java
@@ -3,7 +3,7 @@ public class HTree {
 	
 	public static void main(String[] args) {
 		HTree t=new HTree();
-		t.drawHTree(600,2);
+		t.drawHTree(600,7);
 	}
 	
 	public HTree(){
@@ -19,42 +19,35 @@ public class HTree {
 	dass die Canvas Zeichenrichtung nach unten gerichtet ist. */ 
 	public void drawHTree(int size, int n) {
 		if(n > 0) {
-			c.push();
-
 			c.rotate(90);
-
 			c.moveForward(size/2);
-
-			c.rotate(-90);
-
-			c.moveForward(size/2);
-
+			c.rotate(270);
+			c.drawForward(size/2);
 			c.rotate(180);
-
-			//this.drawHTree(size/2, n - 1);
-
+			c.push();
+			
 			c.drawForward(size);
-
+			c.push();
 			c.rotate(180);
-
 			c.moveForward(size/2);
-
-			c.rotate(-90);
-
+			c.rotate(270);
 			c.drawForward(size);
-
-			c.rotate(-90);
-
+			c.rotate(90);
 			c.moveForward(size/2);
-
 			c.rotate(180);
-
+			c.push();
+			
 			c.drawForward(size);
-
-
-			this.drawHTree(size/2, n - 1);
+			c.rotate(180);
+			c.push();
+			c.pop();
+			drawHTree(size/2, n-1);
+			c.pop();
+			drawHTree(size/2, n-1);
+			c.pop();
+			drawHTree(size/2, n-1);
+			c.pop();
+			drawHTree(size/2, n-1);
 		}
 	}
-	
-	
 }
